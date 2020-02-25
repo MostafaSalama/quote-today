8b8c281a6dc410ab24c8a1c3f03533944edba0c2
Mostafa Salama <mostafalearning2016@gmail.com>
2020-02-25 20:21:31 +0200

    add function to retrive random quote

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {});
 
+function getRandomQuote() {
+	const headers = new Headers();
+	const url = 'https://quotes.rest/quote/random';
+	headers.append('accept', 'application/json');
+	const request = new Request({
+		headers,
+        method:'GET',
+        url,
+	});
+}
