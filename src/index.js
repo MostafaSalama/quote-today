eb22b0550df18ab6df8738f3663040067bd4cf27
Mostafa Salama <mostafalearning2016@gmail.com>
2020-02-25 20:37:44 +0200

    add the function to fetch  a quote

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+	getRandomQuote();
+});
 
 function getRandomQuote() {
 	const headers = new Headers();
-	const url = 'https://quotes.rest/quote/random';
+	const url = 'https://quotes.rest/qod';
 	headers.append('accept', 'application/json');
-	const request = new Request({
+	const request = new Request(url, {
 		headers,
-        method:'GET',
-        url,
+		method: 'GET',
 	});
+	fetch(request)
+		.then(response => {
+			if (response.ok) {
+				return response.text();
+			}
+			console.log(response.status);
+			throw new Error('Error Requesting the server');
+		})
+		.then(result => {
+			createQuote(result);
+		})
+		.catch(err => {
+			console.error(err);
+		});
 }
