75fa0af12ec78ae5cf508d11ee065239429e1b98
Mostafa Salama <mostafalearning2016@gmail.com>
2020-02-25 20:51:35 +0200

    change to a new api url, create app object to change ui and generate new quote

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+const app = {
+	quotes: [],
+	getNewRandomQuote() {
+		const length = this.quotes.length;
+		const randomIndex = Math.floor(Math.random() * length);
+		return this.quotes[randomIndex];
+	},
+	changeQuoteUi() {
+		const p = document.querySelector('blockquote > p');
+		const cite = document.querySelector('blockquote cite');
+		if (app.quotes){
+		    const randomQuote = this.getNewRandomQuote() ;
+		    p.innerText = randomQuote.text ;
+		    cite.innerText = randomQuote.author ;
+        }
+	},
+};
 document.addEventListener('DOMContentLoaded', () => {
 	getRandomQuote();
 });
 
 function getRandomQuote() {
 	const headers = new Headers();
-	const url = 'https://quotes.rest/qod';
+	const url = 'https://type.fit/api/quotes';
 	headers.append('accept', 'application/json');
 	const request = new Request(url, {
 		headers,
@@ -13,13 +30,14 @@ function getRandomQuote() {
 	fetch(request)
 		.then(response => {
 			if (response.ok) {
-				return response.text();
+				return response.json();
 			}
 			console.log(response.status);
 			throw new Error('Error Requesting the server');
 		})
 		.then(result => {
-			createQuote(result);
+			app.quotes = result;
+			app.changeQuoteUi();
 		})
 		.catch(err => {
 			console.error(err);
